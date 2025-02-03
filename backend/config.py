6ed996441a5673df78c508cd9d30423e5cfaf23a
kelteo <kelteo@gmail.com>
2025-02-03 11:43:42 +0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,4 @@
-DATABASE_URL = 'postgresql://postgres:oPPzBpxyfjSGlBzbXlvUjRJmTKQOksGR@postgres.railway.internal:5432/railway'
-SECRET_KEY = '10111213'
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:oPPzBpxyfjSGlBzbXlvUjRJmTKQOksGR@postgres.railway.internal:5432/railway")
+SECRET_KEY = os.getenv("SECRET_KEY", "10111213")
